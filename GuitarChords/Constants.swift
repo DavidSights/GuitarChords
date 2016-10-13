3a0293b080e10d903fdca9e9a55e4458bd3b72a8
David Seitz Jr <davidseitzjr@gmail.com>
2016-10-12 22:23:11 -0500

    Branch: basicSetup - Added segues, segue identifiers along with constants. - Added navigation view controller.

---
+++ GuitarChords/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  GuitarChords
+//
+//  Created by David Seitz Jr on 10/12/16.
+//  Copyright © 2016 David Seitz Jr. All rights reserved.
+//
+
+import Foundation
+
+struct SegueIdentifier {
+
+    static let selectionViewController = "SelectionViewControllerSegueIdentifier"
+    static let chartViewController = "ChartViewControllerSegueIdentifier"
+}
